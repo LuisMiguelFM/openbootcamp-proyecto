0b29c9a1846b182f227e9276e2267247ef438636
LuisMiguelFM <122606609+LuisMiguelFM@users.noreply.github.com>
2023-02-13 19:29:42 +0100

    Curso spring

---
+++ springroot/src/test/java/com/example/springwebshop/SpringwebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
